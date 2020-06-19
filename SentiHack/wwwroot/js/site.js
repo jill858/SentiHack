2a8f6b70070468ed581c594fc95bf70e617ebc00
pehyo <pehyo@LAPTOP-TGRE8DDK>
2020-06-19 20:30:13 +0800

    test

--- SentiHack/wwwroot/js/site.js
+++ SentiHack/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+var url = "https://api.data.gov.sg/v1//environment/air-temperature";
+
+function weather() {
+    $.ajax({
+        url: url,
+        type: "get", //send it through get method
+        data: {
+            date: Date().getTime()
+        },
+        success: function (response) {
+            console.log(response);
+        },
+        error: function (xhr) {
+            console.log(xhr);
+        }
+    });
+}
