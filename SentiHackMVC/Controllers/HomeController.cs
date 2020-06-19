54df1ad0d2b1cc6c23fd62f997dc6e7180828cad
ohichimaru <jun_hong1997@hotmail.com>
2020-06-19 23:45:56 +0800

    yong soon very shuai

--- SentiHackMVC/Controllers/HomeController.cs
+++ SentiHackMVC/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace SentiHackMVC.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Upload()
+        {
+            return View();
+        }
     }
 }
