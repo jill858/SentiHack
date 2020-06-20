eb676641f0dc8ba4d6c7c8b17f7cd760988345ee
pehyo <pehyo@LAPTOP-TGRE8DDK>
2020-06-20 23:04:04 +0800

    upload controller

--- SentiHackMVC/Controllers/HomeController.cs
+++ SentiHackMVC/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RestSharp;
 using SentiHackMVC.Models;
 
 namespace SentiHackMVC.Controllers
@@ -33,11 +34,25 @@ namespace SentiHackMVC.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-
         public IActionResult Upload()
         {
-            client.BaseAddress = new Uri("https://apis.sentient.io/microservices/utility/csvtojson/v1.0/getresults");
+           
             return View();
         }
+       
+         [HttpPost]
+         public IActionResult Upload(HttpPostedFileBase file)
+         {
+             var client = new RestClient("https://apis.sentient.io/microservices/utility/csvtojson/v1.0/getresults");
+             client.Timeout = -1;
+             var request = new RestRequest(Method.POST);
+             request.AddHeader("x-api-key", "C0DD235378A94213B9BF");
+             request.AddHeader("Content-Type", "text/csv");
+             request.AddParameter("text/csv", "<file contents here>", ParameterType.RequestBody);
+             IRestResponse response = client.Execute(request);
+             Console.WriteLine(response.Content);
+             return View();
+         }
+        
     }
 }
